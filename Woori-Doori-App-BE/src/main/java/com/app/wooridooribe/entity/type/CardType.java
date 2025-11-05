59cd2666bbb2554bfb53aca271821c202243f61d
yunkihong-dev <ricky0130@naver.com>
2025-11-05 10:27:33 +0900

    [setting] 프로젝트 폴더명 변경

--- Woori-Doori-App-BE/src/main/java/com/app/wooridooribe/entity/type/CardType.java
+++
@@ -1,5 +0,0 @@
-package com.app.wooridooribe.entity.type;
-
-public enum CardType {
-    CHECK,CREDIT
-}
--- Woori-Doori-App-BE/src/main/java/com/app/wooridooribe/entity/type/YESNO.java
+++
@@ -1,5 +0,0 @@
-package com.app.wooridooribe.entity.type;
-
-public enum YESNO {
-    T,F
-}
