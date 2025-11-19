63c5408e02295ef6dba63015c14081e9b4cd1b9a
ChatHongPT <suk924600@naver.com>
2025-11-19 18:29:50 +0900

    [refactor] #40 - 신규 가입자용 전체 인기 카드 조회 인터페이스 추가

--- Woori-Doori-BE/src/main/java/com/app/wooridooribe/repository/card/CardQueryDsl.java
+++ Woori-Doori-BE/src/main/java/com/app/wooridooribe/repository/card/CardQueryDsl.java
@@ -24,4 +24,13 @@ public interface CardQueryDsl {
      */
     List<com.querydsl.core.Tuple> findPopularCardsByCategory(CategoryType categoryType, List<Long> excludeCardIds,
             int limit);
+
+    /**
+     * 전체 카드 중 tbl_member_card에서 가장 많이 등록된 인기 카드 top 4를 조회합니다
+     * 신규 가입자 추천용
+     * 
+     * @param limit 조회할 최대 개수
+     * @return 카드 ID와 전체 사용자 등록 횟수 Tuple 리스트
+     */
+    List<com.querydsl.core.Tuple> findPopularCardsOverall(int limit);
 }
