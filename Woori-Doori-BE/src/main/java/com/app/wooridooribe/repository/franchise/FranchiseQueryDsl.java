6ac914127bdb7a587b580c73eedac1b1bf65c29c
songhajang <angpan2004@gmail.com>
2025-11-28 10:52:22 +0900

    [feat] #57 - TOP5 가맹점명을 통한 가맹점 로고 조회

---
+++ Woori-Doori-BE/src/main/java/com/app/wooridooribe/repository/franchise/FranchiseQueryDsl.java
@@ -0,0 +1,11 @@
+package com.app.wooridooribe.repository.franchise;
+
+import com.app.wooridooribe.controller.dto.FranchiseFileDto;
+import com.querydsl.core.Tuple;
+
+import java.util.List;
+
+public interface FranchiseQueryDsl {
+    // 가맹점명을 통한 가맹점 로고 조회
+    List<Tuple> findFranchiseFilesByNames(List<String> historyNames);
+}
